b83812bf40751faa9a642b0f53a152e4c6cc13f2
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2025-04-01 11:55:18 -0500

    avance
    muestra mensajes y conxiones especiales

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+/*io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+*/
+/*
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+*/
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 server.listen(3000, () => {
