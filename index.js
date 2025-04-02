66766057965c312ece3699f1d467288ea96c5ff6
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2025-04-01 19:56:33 -0500

    ok
    todo ok, me olvide de subir el comit hahahha

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-/*io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-*/
-/*
 io.on('connection', (socket) => {
   console.log('a user connected');
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+// mensajes en el navegador
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
-*/
+
+// mensajes en el vs
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
@@ -30,3 +31,9 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+
+
+
+
+
